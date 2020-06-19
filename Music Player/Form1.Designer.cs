1266feca67ad83b053f067f6ffb42532927a3307
januszpolak <janusz.polak@interia.pl>
2020-06-19 15:24:52 +0200

    Add project files.

---
+++ Music Player/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Music_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.close = new System.Windows.Forms.PictureBox();
+            this.ListSongs = new System.Windows.Forms.ListBox();
+            this.search = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.minimalize = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimalize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // close
+            // 
+            resources.ApplyResources(this.close, "close");
+            this.close.Name = "close";
+            this.close.TabStop = false;
+            this.close.Click += new System.EventHandler(this.close_Click);
+            // 
+            // ListSongs
+            // 
+            this.ListSongs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ListSongs.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ListSongs.FormattingEnabled = true;
+            resources.ApplyResources(this.ListSongs, "ListSongs");
+            this.ListSongs.Name = "ListSongs";
+            this.ListSongs.SelectedIndexChanged += new System.EventHandler(this.ListSongs_SelectedIndexChanged);
+            // 
+            // search
+            // 
+            this.search.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.search.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(this.search, "search");
+            this.search.ForeColor = System.Drawing.Color.White;
+            this.search.Name = "search";
+            this.search.UseVisualStyleBackColor = false;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // axWindowsMediaPlayer
+            // 
+            resources.ApplyResources(this.axWindowsMediaPlayer, "axWindowsMediaPlayer");
+            this.axWindowsMediaPlayer.Name = "axWindowsMediaPlayer";
+            this.axWindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer.OcxState")));
+            // 
+            // minimalize
+            // 
+            resources.ApplyResources(this.minimalize, "minimalize");
+            this.minimalize.Name = "minimalize";
+            this.minimalize.TabStop = false;
+            this.minimalize.Click += new System.EventHandler(this.minimalize_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.minimalize);
+            this.Controls.Add(this.axWindowsMediaPlayer);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.ListSongs);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minimalize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox close;
+        private System.Windows.Forms.ListBox ListSongs;
+        private System.Windows.Forms.Button search;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer;
+        private System.Windows.Forms.PictureBox minimalize;
+    }
+}
+
---
+++ Music Player/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace Music_Player
+{
+    public partial class Form1 : Form
+    {
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int LPAR);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        const int WM_NCLBUTTONDOWN = 0xA1;
+        const int HT_CAPTION = 0x2;
+
+        private void move_window(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseDown += new MouseEventHandler(move_window);
+        }
+
+        String[] paths, files;
+
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+            
+        }
+
+        private void ListSongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer.URL = paths[ListSongs.SelectedIndex];
+        }
+
+        private void minimalize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "All Supported Audio | *.mp3; *.wma | MP3s | *.mp3 | WMAs | *.wma";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    ListSongs.Items.Add(files[i]);
+
+                }
+            }
+        }
+
+       
+    }
+    
+
+}
